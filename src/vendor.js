081e09b7e4d5465f556cae0855f22de0debd8951
Marjan Georgiev <marjan.georgiev@gmail.com>
2016-07-29 17:39:15 +0200

    Remove output files

--- src/vendor.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-require('@angular/platform-browser');
-require('@angular/platform-browser-dynamic');
-require('@angular/core');
-require('@angular/common');
-require('d3');
-require('moment');
