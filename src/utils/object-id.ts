b6d02bd2c1ba7061ca4e123cb284c850050498b8
Marjan Georgiev <marjan.georgiev@gmail.com>
2016-09-01 16:27:50 +0200

    Remove unused function

--- src/utils/object-id.ts
+++ src/utils/object-id.ts
@@ -3,7 +3,7 @@
 let cache = {};
 
 /**
- * MongoDB Object ID Generation.
+ * Unique Object ID Generation.
  * Example: `571785e57b215b3968f25c3c`
  */
 export default function ObjectId() {
@@ -22,29 +22,3 @@ export default function ObjectId() {
 
   return ObjectId();
 };
-
-/**
- * Generates a short id.
- *
- * Description:
- *  A 4-character alphanumeric sequence (364 = 1.6 million)
- *  This should only be used for JavaScript specific models.
- *  http://stackoverflow.com/questions/6248666/how-to-generate-short-uid-like-ax4j9z-in-js
- *
- *  Example: `ebgf`
- */
-export function ShortId() {
-  /* tslint:disable:no-bitwise */
-  let id = ("0000" + (Math.random() * Math.pow(36, 4) << 0).toString(36)).slice(-4);
-  /* tslint:enable */
-  // append a 'a' because neo gets mad
-  id = `a${id}`;
-
-  // ensure not already used
-  if (!cache[id]) {
-    cache[id] = true;
-    return id;
-  }
-
-  return ShortId();
-}
