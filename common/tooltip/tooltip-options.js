9f84df5308c6c4b0e266eb5ae00061625119e9fc
Marjan Georgiev <marjan.georgiev@gmail.com>
2016-11-07 12:17:36 -0500

    (deploy): CI

--- common/tooltip/tooltip-options.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var TooltipOptions = (function () {
-    function TooltipOptions(opts) {
-        Object.assign(this, opts);
-    }
-    return TooltipOptions;
-}());
-exports.TooltipOptions = TooltipOptions;
-//# sourceMappingURL=tooltip-options.js.map
