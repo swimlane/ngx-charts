34e63e2855c9ddf96b81f9fd93eafa3c17ecf49e
Austin McDaniel <amcdaniel2@gmail.com>
2016-12-05 10:24:58 -0500

    (util): adding throttle for 2.3 upgrade

--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,62 +1,11 @@
-﻿export function debounce(func, wait, immediate) {
-  let timeout, args, context, timestamp, result;
-  return function() {
-    context = this;
-    args = arguments;
-    timestamp = new Date();
-    let later = function() {
-      let last: any = new Date().getTime() - timestamp;
-      if (last < wait) {
-        timeout = setTimeout(later, wait - last);
-      } else {
-        timeout = null;
-        if (!immediate) {
-          result = func.apply(context, args);
-        }
-      }
-    };
-    let callNow = immediate && !timeout;
-    if (!timeout) {
-      timeout = setTimeout(later, wait);
-    }
-    if (callNow) {
-      result = func.apply(context, args);
-    }
-    return result;
-  };
-}
-
-/**
- * Debounce decorator
- *
- *  class MyClass {
- *    debounceable(10)
- *    myFn() { ... }
- *  }
- */
-export function debounceable(duration, immediate) {
-  return function innerDecorator(target, key, descriptor) {
-    return {
-      configurable: true,
-      enumerable: descriptor.enumerable,
-      get: function getter() {
-        Object.defineProperty(this, key, {
-          configurable: true,
-          enumerable: descriptor.enumerable,
-          value: debounce(descriptor.value, duration, immediate)
-        });
-
-        return this[key];
-      }
-    };
-  };
-}
-
-/**
+﻿/**
  * Throttle a function
- * @param  {any}    func    function to execute
- * @param  {number} wait    duration to wait
- * @param  {any}    options options
+ * 
+ * @export
+ * @param {*} func
+ * @param {number} wait
+ * @param {*} [options]
+ * @returns
  */
 export function throttle(func: any, wait: number, options?: any) {
   options = options || {};
@@ -103,8 +52,13 @@ export function throttle(func: any, wait: number, options?: any) {
  *    throttleable(10)
  *    myFn() { ... }
  *  }
+ * 
+ * @export
+ * @param {number} duration
+ * @param {*} [options]
+ * @returns
  */
-export function throttleable (duration: number, options?: any) {
+export function throttleable(duration: number, options?: any) {
   return function innerDecorator (target, key, descriptor) {
     return {
       configurable: true,
