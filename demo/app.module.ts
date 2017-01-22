fd95d9fbb78ff451f711744f987875845ea01c17
Marjan Georgiev <marjan.georgiev@gmail.com>
2017-01-22 15:30:42 +0100

    Dynamically set base location to demo page

--- demo/app.module.ts
+++ demo/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { APP_BASE_HREF, Location } from '@angular/common';
 import { AppComponent } from './app.component';
 import { NgxChartsModule } from '../src';
 
 @NgModule({
+  providers: [
+    {
+      provide: APP_BASE_HREF,
+      useFactory: getBaseLocation
+    }
+  ],
   imports: [NgxChartsModule, BrowserModule, FormsModule],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function getBaseLocation() {
+    let paths: string[] = location.pathname.split('/').splice(1, 1);
+    let basePath: string = (paths && paths[0]) || '';
+    return '/' + basePath;
+}
