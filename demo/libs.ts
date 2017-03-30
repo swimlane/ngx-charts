c3120af830dd7f49afcf40113b8c02e7ab213d1f
clydin <clydin@users.noreply.github.com>
2017-03-30 09:02:02 -0400

    fix(demo): polyfill browsers without animation support (#304)

--- demo/libs.ts
+++ demo/libs.ts
@@ -7,6 +7,7 @@ import { disableDebugTools } from '@angular/platform-browser';
 import { enableProdMode } from '@angular/core';
 import '@angular/platform-browser-dynamic';
 import '@angular/common';
+import 'web-animations-js';
 
 let _decorateModuleRef = function identity<T>(value: T): T { return value; };
 
