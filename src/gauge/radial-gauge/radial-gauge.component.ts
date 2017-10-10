f1c2849d28f83da44086a88ac5a122c562a085f6
Piotr Ptak <piotrptak@outlook.com>
2017-10-10 21:22:15 +0200

    added fix to the method generating arcs

--- src/gauge/radial-gauge/radial-gauge.component.ts
+++ src/gauge/radial-gauge/radial-gauge.component.ts
@@ -23,17 +23,34 @@ export class RadialGaugeComponent extends BaseChartComponent implements OnInit, 
   private maxValue = 100;
   private minAngle = -90;
   private maxAngle = 90;
+  private segments = [
+    // {
+    //   minValue: 0,
+    //   maxValue: 30,
+    //   color: 'red'
+    // },
+    // {
+    //   minValue: 30,
+    //   maxValue: 60,
+    //   color: 'yellow'
+    // },
+    // {
+    //   minValue: 60,
+    //   maxValue: 100,
+    //   color: 'green'
+    // }
+  ];
 
   private scale: ScaleLinear<number, number>;
   private ticks: number[];
   private tickData: number[];
-  private range: number;
+  private degreeRange: number;
 
   ngOnInit(): void {
     this.scale = this.getScale();
     this.ticks = this.scale.ticks(this.majorTicks);
     this.tickData = range(this.majorTicks).map(() => { return 1 / this.majorTicks; });
-    this.range = this.getRange();
+    this.degreeRange = this.getDegreeRange();
     this.arcs = this.getArcs();
 
     this.translate = 'translate(150,150)';
@@ -44,52 +61,79 @@ export class RadialGaugeComponent extends BaseChartComponent implements OnInit, 
     setTimeout(() => {
       console.log('afterViewInit');
 
-      console.log(this.newAngle(10));
-      console.log(this.newAngle(20));
+      console.log('ticks', this.ticks);
+      console.log('ticksData', this.tickData);
+      console.log('degreeRange', this.degreeRange);
+      console.log('arcs', this.arcs);
+      console.log('newangle', this.newAngle(0));
     });
   }
 
-  public startAngle(d, i) {
-    const min = this.minAngle;
-    const max = this.maxAngle;
-    return (min + (d * i * (max - min))) * Math.PI / 180;
-    // const ratio = d * i;
-    // return this.deg2rad(this.minAngle + (ratio * this.getRange()));
-  }
+  // public startAngle(value: number, index: number) {
 
-  public endAngle(d, i) {
-    const min = this.minAngle;
-    const max = this.maxAngle;
-    return (min + (d * (i + 1) * (max - min))) * Math.PI / 180;
-    // const ratio = d * (i + 1);
-    // return this.deg2rad(this.minAngle + (ratio * this.getRange()));
-  }
+  //   // const ratio = d * i;
+  //   // return this.deg2rad(this.minAngle + (ratio * this.getRange()));
+  // }
+
+  // public endAngle(d, i) {
+  //    const ratio = d * (i + 1);
+  //    return this.deg2rad(this.minAngle + (ratio * this.getRange()));
+  // }
 
   private getArcs(): any {
     const result = [];
 
-    const arc = {
-      startAngle: 0,
-      endAngle: 3,
-      innerRadius: 20,
-      outerRadius: 40
-    };
-    result.push(arc);
+    if (this.segments != null && this.segments.length > 0) {
+      for(const i in this.segments) {
+        const arc = {
+          startAngle: this.getSegmentAngle(this.segments[i].minValue),
+          endAngle: this.getSegmentAngle(this.segments[i].maxValue),
+          innerRadius: 60,
+          outerRadius: 80
+        };
+        result.push(arc);
+      }
+    } else {
+      for(let i = 0; i < this.majorTicks; i++) {
+        const arc = {
+          startAngle: this.getTickAngle(i),
+          endAngle: this.getTickAngle(i + 1),
+          innerRadius: 60,
+          outerRadius: 80
+        };
+        result.push(arc);
+      }
+    }
 
     return result;
+  }
+
+  private getSegmentAngle(value: number): number {
+    const degree =  this.minAngle + (value * this.getDegreeRange() / this.getValueRange());
+    return this.deg2rad(degree);
+  }
+
+  private getTickAngle(index: number): number {
+    const degree = this.minAngle + (this.ticks[index] * this.getDegreeRange() / this.getValueRange());
+    return this.deg2rad(degree);
   }
 
   private deg2rad(degree: number): number {
     return degree * Math.PI / 180;
   }
 
-  private getRange(): number {
+  private getDegreeRange(): number {
     return this.maxAngle - this.minAngle;
+  }
+
+  private getValueRange(): number {
+    return this.maxValue - this.minValue;
   }
 
   private newAngle(angle: number): number {
     const ratio = this.scale(angle);
-    const newAngle = this.minAngle + (ratio * this.getRange());
+    console.log(ratio);
+    const newAngle = this.minAngle + (ratio * this.getDegreeRange());
     return newAngle;
   }
 
