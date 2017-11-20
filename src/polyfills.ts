d51247238eff6e828533ad7c085e34e1c5bd5790
Johann Rekowski <johann.rekowski@gmail.com>
2017-11-20 15:11:33 +0100

    (fix): Export polyfills to fix AoT build issue with Angular 5 (#644)

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,11 +1,15 @@
-// IE11 fix
-// Ref: https://github.com/swimlane/ngx-charts/issues/386
-if (typeof(SVGElement) !== 'undefined' && typeof SVGElement.prototype.contains === 'undefined') {
-  SVGElement.prototype.contains = HTMLDivElement.prototype.contains;
+// The export is needed here to generate a valid polyfills.metadata.json file
+export function ngxChartsPolyfills() {
+  // IE11 fix
+  // Ref: https://github.com/swimlane/ngx-charts/issues/386
+  if (typeof(SVGElement) !== 'undefined' && typeof SVGElement.prototype.contains === 'undefined') {
+    SVGElement.prototype.contains = HTMLDivElement.prototype.contains;
+  }
+  // IE11 fix (http://caniuse.com/#search=classList)
+  if (!('classList' in document.createElementNS('http://www.w3.org/2000/svg', 'g'))) {
+    const descr = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'classList');
+    Object.defineProperty(SVGElement.prototype, 'classList', descr);
+  }
 }
 
-// IE11 fix (http://caniuse.com/#search=classList)
-if (!('classList' in document.createElementNS('http://www.w3.org/2000/svg', 'g'))) {
-  const descr = Object.getOwnPropertyDescriptor(HTMLElement.prototype, 'classList');
-  Object.defineProperty(SVGElement.prototype, 'classList', descr);
-}
+ngxChartsPolyfills();
