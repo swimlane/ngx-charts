c7261bfaa0e187478886d57f7c2a494faf412f8c
Emil Petkov <38661508+emilkpetkov@users.noreply.github.com>
2018-05-30 16:43:07 +0300

    Update webpack.common.js

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -40,7 +40,7 @@ module.exports = function(options = {}) {
         },
         {
           test: /\.css/,
-          loader: [
+          use: [
             ExtractTextPlugin.extract({
               fallback: 'style-loader',
               use: 'css-loader'
@@ -52,7 +52,7 @@ module.exports = function(options = {}) {
         },
         {
           test: /\.scss$/,
-          loader: [
+          use: [
             ExtractTextPlugin.extract({
               fallback: 'style-loader',
               use: 'css-loader'
