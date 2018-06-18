b437ac6c9ba03393906545429001c29df53bb73a
Emil Petkov <38661508+emilkpetkov@users.noreply.github.com>
2018-06-18 10:35:25 +0300

    Update webpack.dev.js

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -48,7 +48,7 @@ module.exports = function(options) {
         },
         {
           test: /\.ts$/,
-          use: [
+          loaders: [
             'awesome-typescript-loader',
             'angular2-template-loader'
           ],
