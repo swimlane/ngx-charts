3a41ad6b27514ac5dac4c54ee14c50c69c402ea2
Alex Povar <1074182+zvirja@users.noreply.github.com>
2018-07-17 18:04:09 +0300

    Remove duplicated exports (#919)
    Currently all the components are exported via index.ts files.
    There is no need to export them additionally via modules.

--- src/bubble-chart/bubble-chart.module.ts
+++ src/bubble-chart/bubble-chart.module.ts
@@ -3,8 +3,6 @@ import { ChartCommonModule } from '../common/chart-common.module';
 import { BubbleChartComponent } from './bubble-chart.component';
 import { BubbleSeriesComponent } from './bubble-series.component';
 
-export { BubbleChartComponent, BubbleSeriesComponent };
-
 @NgModule({
   imports: [ChartCommonModule],
   declarations: [
--- src/heat-map/heat-map.module.ts
+++ src/heat-map/heat-map.module.ts
@@ -4,8 +4,6 @@ import { HeatMapCellComponent } from './heat-map-cell.component';
 import { HeatCellSeriesComponent } from './heat-map-cell-series.component';
 import { HeatMapComponent } from './heat-map.component';
 
-export { HeatMapCellComponent, HeatCellSeriesComponent, HeatMapComponent };
-
 @NgModule({
   imports: [ChartCommonModule],
   declarations: [
