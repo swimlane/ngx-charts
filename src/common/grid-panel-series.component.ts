c64c05602119f172cfab69c8f5da7c1a247dc2d7
Dirk van Vugt <dirkvanvugt@CMs-iMac-Tjeerd.cm.local>
2018-10-31 16:32:18 +0100

    Remove unused param in GridPanelSeriesComponent

--- src/common/grid-panel-series.component.ts
+++ src/common/grid-panel-series.component.ts
@@ -42,7 +42,7 @@ export class GridPanelSeriesComponent implements OnChanges {
   }
 
   getGridPanels(): any[] {
-    return this.data.map((d, i) => {
+    return this.data.map((d) => {
       let offset;
       let width;
       let height;
