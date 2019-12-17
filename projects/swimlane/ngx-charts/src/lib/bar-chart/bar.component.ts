fb18ece7d5a57c38fc26a0574f4b1ad7ffb2e9bc
KingDarBoja <mbojato@uninorte.edu.co>
2019-12-17 10:48:35 -0500

    Apply prettier formatting to bar component

--- projects/swimlane/ngx-charts/src/lib/bar-chart/bar.component.ts
+++ projects/swimlane/ngx-charts/src/lib/bar-chart/bar.component.ts
@@ -33,7 +33,7 @@ import { transition } from 'd3-transition';
       [class.hidden]="hideBar"
       [attr.d]="path"
       [attr.aria-label]="ariaLabel"
-      [attr.fill]="(hasGradient ? gradientFill : fill)"
+      [attr.fill]="hasGradient ? gradientFill : fill"
       (click)="select.emit(data)"
     />
   `,
@@ -191,7 +191,7 @@ export class BarComponent implements OnChanges {
       const n0 = path0.getTotalLength();
       // tslint:disable-next-line: ban-comma-operator
       const n1 = (path1.setAttribute('d', d1), path1).getTotalLength();
-   
+
       // Uniform sampling of distance based on specified precision.
       const distances = [0];
       let i = 0;
@@ -201,20 +201,27 @@ export class BarComponent implements OnChanges {
         i += dt;
       }
       distances.push(1);
-   
+
       // Compute point-interpolators at each distance.
       const points = distances.map(function(t) {
         const p0 = path0.getPointAtLength(t * n0);
         const p1 = path1.getPointAtLength(t * n1);
         return interpolate([p0.x, p0.y], [p1.x, p1.y]);
       });
-   
+
       return function(t) {
-        return t < 1 ? 'M' + points.map(function(p) { return p(t); }).join('L') : d1;
+        return t < 1
+          ? 'M' +
+              points
+                .map(function(p) {
+                  return p(t);
+                })
+                .join('L')
+          : d1;
       };
     };
   }
-  
+
   getRadius(): number {
     let radius = 0;
 
