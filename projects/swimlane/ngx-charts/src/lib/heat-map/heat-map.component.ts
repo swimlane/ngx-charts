78d433ca6788599de6c782f8a586ba4520730793
Adam Stauffer <adam.r.stauffer@gmail.com>
2020-05-02 18:24:02 -0500

    add customColors support to heatmap

--- projects/swimlane/ngx-charts/src/lib/heat-map/heat-map.component.ts
+++ projects/swimlane/ngx-charts/src/lib/heat-map/heat-map.component.ts
@@ -286,7 +286,7 @@ export class HeatMapComponent extends BaseChartComponent {
   }
 
   setColors(): void {
-    this.colors = new ColorHelper(this.scheme, this.scaleType, this.valueDomain);
+    this.colors = new ColorHelper(this.scheme, this.scaleType, this.valueDomain, this.customColors);
   }
 
   getLegendOptions() {
