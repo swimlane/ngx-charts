3b31956e1c887ccf25d53a5e54ebc6b69a7a5a34
Jayson Harshbarger <jayson.harshbarger@swimlane.com>
2022-07-22 11:52:37 -0600

    merge and lint

--- projects/swimlane/ngx-charts/src/lib/box-chart/box.component.ts
+++ projects/swimlane/ngx-charts/src/lib/box-chart/box.component.ts
@@ -280,13 +280,8 @@ export class BoxComponent implements OnChanges {
 
     const lineCoordinates: LineCoordinates = cloneDeep(this.lineCoordinates);
 
-    lineCoordinates[1].v1.y =
-      lineCoordinates[1].v2.y =
-      lineCoordinates[3].v1.y =
-      lineCoordinates[3].v2.y =
-      lineCoordinates[0].v1.y =
-      lineCoordinates[0].v2.y =
-        lineCoordinates[2].v1.y;
+    lineCoordinates[1].v1.y = lineCoordinates[1].v2.y = lineCoordinates[3].v1.y = lineCoordinates[3].v2.y = lineCoordinates[0].v1.y = lineCoordinates[0].v2.y =
+      lineCoordinates[2].v1.y;
 
     return lineCoordinates;
   }
