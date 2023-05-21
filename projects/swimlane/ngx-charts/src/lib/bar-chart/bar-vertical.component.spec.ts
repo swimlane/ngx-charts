a3f6c45310ca8c696fadbe3a02fec7cb14525a4d
Surya Pabbineedi <suryanarayana.pabbineedi@swimlane.com>
2023-05-21 17:53:23 +0530

    feat(wrap-ticks): unit tests

--- projects/swimlane/ngx-charts/src/lib/bar-chart/bar-vertical.component.spec.ts
+++ projects/swimlane/ngx-charts/src/lib/bar-chart/bar-vertical.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed } from '@angular/core/testing';
-import { Component } from '@angular/core';
+import { TestBed, tick } from '@angular/core/testing';
+import { Component, DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -8,6 +8,7 @@ import { APP_BASE_HREF } from '@angular/common';
 
 import { BarChartModule } from './bar-chart.module';
 import { BarComponent } from './bar.component';
+import { XAxisTicksComponent } from '../common/axes/x-axis-ticks.component';
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 
@@ -124,6 +125,104 @@ describe('<ngx-charts-bar-vertical>', () => {
       const bar = fixture.debugElement.query(By.directive(BarComponent));
 
       expect(bar.componentInstance.width).toEqual(43); // ~(360 - 5 * barPadding) / 6
+    });
+  });
+
+  describe('x-axis - wrap ticks', () => {
+    const getContent = (axisTick: DebugElement) =>
+      axisTick.queryAll(By.css('tspan')).map(entry => entry.nativeElement.textContent.trim());
+
+    it('should wrap tick if there is available space', () => {
+      TestBed.overrideComponent(TestComponent, {
+        set: {
+          template: `
+          <ngx-charts-bar-vertical
+            [animations]="false"
+            [view]="[400, 300]"
+            [scheme]="colorScheme"
+            [results]="[
+              { name: 'Lorem Ipsum', value: 40632 },
+              { name: 'Lorem Ipsum is simply', value: 50000 },
+              { name: 'Lorem Ipsum is simply dummy text', value: 36240 },
+              { name: 'Lorem Ipsum is simply dummy text of the printing', value: 3000 },
+              { name: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry', value: 5655 },
+            ]"
+            [xAxis]="true"
+            [yAxis]="true"
+            [wrapTicks]="true">
+          </ngx-charts-bar-vertical>`
+        }
+      }).compileComponents();
+
+      const fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+
+      const xAxisTicks = fixture.debugElement.query(By.directive(XAxisTicksComponent));
+      const axisTicks = xAxisTicks.queryAll(By.css('g.tick > text'));
+
+      expect(axisTicks.length).toEqual(5);
+
+      expect(axisTicks[0].queryAll(By.css('tspan')).length).toEqual(1);
+      expect(getContent(axisTicks[0])).toEqual(['Lorem Ipsum']);
+
+      expect(axisTicks[1].queryAll(By.css('tspan')).length).toEqual(2);
+      expect(getContent(axisTicks[1])).toEqual(['Lorem Ipsum is', 'simply']);
+
+      expect(axisTicks[2].queryAll(By.css('tspan')).length).toEqual(3);
+      expect(getContent(axisTicks[2])).toEqual(['Lorem Ipsum is', 'simply dummy', 'text']);
+
+      expect(axisTicks[3].queryAll(By.css('tspan')).length).toEqual(4);
+      expect(getContent(axisTicks[3])).toEqual(['Lorem Ipsum is', 'simply dummy', 'text of the', 'printing']);
+
+      expect(axisTicks[4].queryAll(By.css('tspan')).length).toEqual(5);
+      expect(getContent(axisTicks[4])).toEqual([
+        'Lorem Ipsum is',
+        'simply dummy',
+        'text of the',
+        'printing and',
+        'typesetting...'
+      ]);
+    });
+
+    it('should show a max of 5 lines for a wrapped tick', () => {
+      TestBed.overrideComponent(TestComponent, {
+        set: {
+          template: `
+          <ngx-charts-bar-vertical
+            [animations]="false"
+            [view]="[400, 300]"
+            [scheme]="colorScheme"
+            [results]="[
+              { name: 'Lorem Ipsum', value: 40632 },
+              { name: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s', value: 5655 },
+            ]"
+            [xAxis]="true"
+            [yAxis]="true"
+            [wrapTicks]="true"
+          >
+          </ngx-charts-bar-vertical>`
+        }
+      }).compileComponents();
+
+      const fixture = TestBed.createComponent(TestComponent);
+      fixture.detectChanges();
+
+      const xAxisTicks = fixture.debugElement.query(By.directive(XAxisTicksComponent));
+      const axisTicks = xAxisTicks.queryAll(By.css('g.tick > text'));
+
+      expect(axisTicks.length).toEqual(2);
+
+      expect(axisTicks[0].queryAll(By.css('tspan')).length).toEqual(1);
+      expect(getContent(axisTicks[0])).toEqual(['Lorem Ipsum']);
+
+      expect(axisTicks[1].queryAll(By.css('tspan')).length).toEqual(5);
+      expect(getContent(axisTicks[1])).toEqual([
+        'Lorem Ipsum is simply dummy',
+        'text of the printing and',
+        'typesetting industry. Lorem',
+        'Ipsum has been the industrys',
+        'standard dummy text ever...'
+      ]);
     });
   });
 });
