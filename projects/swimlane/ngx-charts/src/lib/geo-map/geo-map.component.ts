c4998c0d38b838b6f6a5ebc6f42606fe737fbfe5
Eric Moore <EricGittaMoore@duck.com>
2024-05-12 22:05:52 +0800

    feat: geo-map

---
+++ projects/swimlane/ngx-charts/src/lib/geo-map/geo-map.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-charts-geo-map',
+  template: ``
+})
+export class GeoMapComponent {}
---
+++ projects/swimlane/ngx-charts/src/lib/geo-map/geo-map.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { GeoMapComponent } from '@swimlane/ngx-charts/geo-map/geo-map.component';
+
+@NgModule({
+  declarations: [GeoMapComponent],
+  exports: [GeoMapComponent]
+})
+export class GeoMapModule {}
