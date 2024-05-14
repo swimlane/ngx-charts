bf08065a4e7798f6cc85252d9cba369a7ac4a147
Eric Moore <EricGittaMoore@duck.com>
2024-05-14 21:05:15 +0800

    fix: geo-map

--- projects/swimlane/ngx-charts/src/lib/geo-map/geo-map.component.ts
+++ projects/swimlane/ngx-charts/src/lib/geo-map/geo-map.component.ts
@@ -1,7 +1,76 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ContentChild,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+  ViewEncapsulation
+} from '@angular/core';
+import { BaseChartComponent } from '@swimlane/ngx-charts/common/base-chart.component';
+import { LegendOptions, LegendPosition } from '@swimlane/ngx-charts/common/types/legend.model';
+import { DataItem } from '@swimlane/ngx-charts/models/chart-data.model';
+import { ColorHelper } from '@swimlane/ngx-charts/common/color.helper';
+import { ScaleType } from '@swimlane/ngx-charts/common/types/scale-type.enum';
+import { ViewDimensions } from '@swimlane/ngx-charts/common/types/view-dimension.interface';
 
 @Component({
   selector: 'ngx-charts-geo-map',
-  template: ``
+  template: ``,
+  styleUrls: ['../common/base-chart.component.scss', './geo-map.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GeoMapComponent {}
+export class GeoMapComponent extends BaseChartComponent {
+  @Input() labels: boolean = false;
+  @Input() legend: boolean = false;
+  @Input() legendTitle: string = 'Legend';
+  @Input() legendPosition: LegendPosition = LegendPosition.Right;
+  @Input() explodeSlices: boolean = false;
+  @Input() doughnut: boolean = false;
+  @Input() arcWidth: number = 0.25;
+  @Input() gradient: boolean;
+  @Input() activeEntries: any[] = [];
+  @Input() tooltipDisabled: boolean = false;
+  @Input() labelFormatting: any;
+  @Input() trimLabels: boolean = true;
+  @Input() maxLabelLength: number = 10;
+  @Input() tooltipText: any;
+  @Output() dblclick = new EventEmitter();
+  // optional margins
+  @Input() margins: number[];
+  @Output() select = new EventEmitter();
+  @Output() activate = new EventEmitter();
+  @Output() deactivate = new EventEmitter();
+
+  @ContentChild('tooltipTemplate') tooltipTemplate: TemplateRef<any>;
+
+  data: DataItem[];
+  colors: ColorHelper;
+  domain: string[];
+  dims: ViewDimensions;
+  legendOptions: LegendOptions;
+
+  getDomain(): string[] {
+    return this.results.map(d => d.label);
+  }
+
+  onClick(data: DataItem | string): void {
+    this.select.emit(data);
+  }
+
+  setColors(): void {
+    this.colors = new ColorHelper(this.scheme, ScaleType.Ordinal, this.domain, this.customColors);
+  }
+
+  getLegendOptions(): LegendOptions {
+    return {
+      scaleType: ScaleType.Ordinal,
+      domain: this.domain,
+      colors: this.colors,
+      title: this.legendTitle,
+      position: this.legendPosition
+    };
+  }
+}
--- projects/swimlane/ngx-charts/src/lib/geo-map/geo-map.module.ts
+++ projects/swimlane/ngx-charts/src/lib/geo-map/geo-map.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { GeoMapComponent } from '@swimlane/ngx-charts/geo-map/geo-map.component';
+import { ChartCommonModule } from '@swimlane/ngx-charts/common/chart-common.module';
 
 @NgModule({
+  imports: [ChartCommonModule],
   declarations: [GeoMapComponent],
   exports: [GeoMapComponent]
 })
